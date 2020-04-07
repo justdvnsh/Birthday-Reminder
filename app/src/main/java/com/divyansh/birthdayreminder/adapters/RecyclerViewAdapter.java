c8b99aed7ab7366ccdc078d8d78c8d7432d23ff5
justdvnsh <justdvnsh2208@gmail.com>
2020-04-07 13:05:02 +0530

    added recycler view and adapter and view model

--- app/src/main/java/com/divyansh/birthdayreminder/adapters/RecyclerViewAdapter.java
+++ app/src/main/java/com/divyansh/birthdayreminder/adapters/RecyclerViewAdapter.java
@@ -1,31 +1,79 @@
 package com.divyansh.birthdayreminder.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.divyansh.birthdayreminder.R;
+import com.divyansh.birthdayreminder.data.Entities.Event;
+
+import java.util.List;
+
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder> {
+
+    private List<Event> birthdayList;
+    private mOnItemClickListener listener;
+
+    public interface mOnItemClickListener {
+        void onItemClick(Event event);
+    }
+
+    public RecyclerViewAdapter(List<Event> birthdayList, mOnItemClickListener listener) {
+        this.birthdayList = birthdayList;
+        this.listener = listener;
+    }
+
     @NonNull
     @Override
     public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new RecyclerViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recycler_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
-
+        Event event = birthdayList.get(position);
+        holder.postedOn.setText(event.getDate().toString());
+        holder.title.setText(event.getName());
+        holder.description.setText(event.getDescription());
+        holder.daysRemaning.setText(String.valueOf(event.getDaysUntil()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (birthdayList.size() == 0) return 0;
+        return birthdayList.size();
     }
 
-    public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+    public void addBithdays(List<Event> birthdayList) {
+        this.birthdayList = birthdayList;
+        notifyDataSetChanged();
+    }
+
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView postedOn;
+        TextView title;
+        TextView description;
+        TextView daysRemaning;
+
         public RecyclerViewHolder(@NonNull View itemView) {
             super(itemView);
+            itemView.setOnClickListener(this);
+
+            postedOn = itemView.findViewById(R.id.publish_date);
+            title = itemView.findViewById(R.id.title);
+            description = itemView.findViewById(R.id.content);
+            daysRemaning = itemView.findViewById(R.id.time_left);
+        }
+
+        @Override
+        public void onClick(View v) {
+
         }
     }
 }
--- app/src/main/java/com/divyansh/birthdayreminder/ui/MainActivity.java
+++ app/src/main/java/com/divyansh/birthdayreminder/ui/MainActivity.java
@@ -1,16 +1,51 @@
 package com.divyansh.birthdayreminder.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.divyansh.birthdayreminder.R;
+import com.divyansh.birthdayreminder.adapters.RecyclerViewAdapter;
+import com.divyansh.birthdayreminder.data.Entities.Event;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements RecyclerViewAdapter.mOnItemClickListener {
+
+    private BirthdayViewModel viewModel;
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new RecyclerViewAdapter(new ArrayList<Event>(), this);
+        recyclerView.setAdapter(adapter);
+
+        viewModel = ViewModelProviders.of(this).get(BirthdayViewModel.class);
+        viewModel.getAllEvents().observe(MainActivity.this, new Observer<List<Event>>() {
+            @Override
+            public void onChanged(List<Event> events) {
+                adapter.addBithdays(events);
+            }
+        });
+    }
+
+    @Override
+    public void onItemClick(Event event) {
+
     }
 }
